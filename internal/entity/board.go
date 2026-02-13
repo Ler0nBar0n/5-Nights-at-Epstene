07826bf568a9c1f824b64cc121ae14d6516bb21a
LeronBaron <kotixxx6@gmail.com>
2026-02-13 19:06:30 +0400

    Сделал ентити классы и структуру

---
+++ internal/entity/board.go
@@ -0,0 +1,12 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type Board struct {
+	gorm.Model
+
+	Name      string `gorm:"type:varchar(100);not null;unique" json:"name"`
+	CreatorId uint   `json:"creator_id"`
+}
---
+++ internal/go.mod
@@ -0,0 +1,3 @@
+module nights-at-epstene
+
+go 1.25.0
